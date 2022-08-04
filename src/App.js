6136ca9b6c4890cf2dd5fbd1a3b124d1137fd987
Hubert Ferrer Guerrero <hubert.ferrerg8@gmail.com>
2022-08-03 22:51:42 -0400

    Se guardan cambios en el fetch

--- src/App.js
+++ src/App.js
@@ -4,8 +4,21 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Navbar from './components/navegacion/Navbar'
 import ItemList from './components/ItemList/ItemList';
 import ItemCount from './components/ItemCount/ItemCount';
+import traerDatos from './data/data';
+import { useEffect } from 'react';
 
 function App() {
+const[data,setData] = useState([])
+const[loading,setLoading] = useState(true)
+
+useEffect(()=>{
+  traerDatos
+  .then((resp)=>setData(resp))
+  .catch(err=>console.log(err))
+  .finally(()=>setLoading(false))
+},[])
+
+
   return (
     <div className="App">
      <Router>
---
+++ src/data/data.js
@@ -0,0 +1,45 @@
+
+
+ const data=[{
+        
+            nombre: "God of war4",
+            consola: "ps4",
+            id: "1",
+            image: "https://home.ripley.cl/store/Attachment/WOP/D172/2000369090375/2000369090375_2.jpg",
+            stock: 10
+            
+   
+        },
+        {
+            nombre: "Sony Dragon Quest Xi: Echoes Of An Elusive Age",
+            consola: "ps4",
+            id: "2",
+            image: "https://www.hites.com/dw/image/v2/BDPN_PRD/on/demandware.static/-/Sites-mastercatalog_HITES/default/dw474d6dd1/images/original/pim/845304001/845304001_1.jpg?sw=1000&sh=1000",
+            stock: 15
+        },
+        {
+            nombre: "Dragon Ball Fighterz Standard Edition",
+            consola: "ps4",
+            id: "3",
+            image: "https://i.linio.com/p/0162f86bd3c5e0a3fc3088fe1c061575-product.webp",
+            stock: 13
+        }
+    ]
+
+    const traerDatos = new Promise((resolve, reject)=> {
+        let condition = true
+        if(condition){
+            setTimeout(() => {
+                resolve(data)
+            },2000);
+                
+            
+        }else{
+            reject(console.log("No existen datos"))
+        }
+    })
+
+    export default traerDatos
+    
+
+    
